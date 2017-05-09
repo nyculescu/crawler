37e669da437edcef5170a6db42842df6686ca12b
nyculescu <cn.cata@gmail.com>
2017-05-10 00:11:11 +0300

    Ver 0.2.0 - stable | pcgarage is working with cyclicity | emag will be implemented

--- crawler_main.js
+++ crawler_main.js
@@ -23,7 +23,8 @@ var winston = require('winston');
 // File I/O is provided by simple wrappers around standard POSIX functions. All the methods have async and sync forms.
 var fs = require('fs');
 // Parse the config.json file
-var config = require('./config.json');
+var config_pcgarage = require('./pages/config_pcgar.json'); //@todo to be renamed!
+var config_emag = require('./pages/config_emg.json'); //@todo to be renamed!
 // Global variable used for write into a file log - can be made local var in the future
 var logger;
 
@@ -32,61 +33,46 @@ var logger;
 //var webpage_body;
 //--------------------------------------------------------------
 
+var crawl__pcgarage_cnt = 0;
+var crawl__emag_cnt = 0;
+var crawl__call_interval_pcgarage;
+var crawl__call_interval_emag;
+
 /**
  * This is the main function of this file
  */
-function crawl() {
-    setTimeout(function() {
+function crawl__main() {
+    var interval = 2000; //2 seconds
 
-        if (config !== undefined || config.sites !== undefined) {
-            //Search through sites
-            for (i = 0; i < config.sites.length; i++) {
-                let domain_name = config.sites[i].name;
-                let base_url;
-                let product_type;
+    if (crawl__check_config_files()) {
+        setTimeout(function() {
+            crawl__call_interval_pcgarage = setInterval(function() {
+                crawl__pcgarage_cnt++;
 
-                //Search through site's elements
-                for (j = 0; j < config.sites[i].map.length; j++) {
-                    base_url = config.sites[i].url + config.sites[i].map[j].url;
-                    product_type = config.sites[i].map[j].product_type;
-                    //It's a good idea to have a delay between requests
-                    /*delay(5000).then(() => {
-                            if (domain_name !== undefined || base_url !== undefined || product_type !== undefined) {
-                                console.log('\n---' + base_url + '---\n');
-                                crawl__establish_connection_then_parse(domain_name, base_url, product_type);
-                            } else {
-                                if (domain_name === undefined)
-                                    LOG__to_console_and_file('e', domain_name, 'crawl() | domain_name undefined');
-                                if (base_url === undefined)
-                                    LOG__to_console_and_file('e', domain_name, 'crawl() | base_url undefined');
-                                if (product_type === undefined)
-                                    LOG__to_console_and_file('e', domain_name, 'crawl() | product_type undefined');
-                            }
-                        })
-                        .catch(err => {
-                            LOG__to_console_and_file('e', domain_name, 'crawl() | Error on delay');
-                        });*/
-
-                    //  call a 3s setTimeout when the loop is called
-                    console.log('\n---' + base_url + '---\n');
-
-                    j++; //  increment the counter
-                    if (j < config.sites[i].map.length) { //  if the counter < 10, call the loop function
-                        console.log('plm');
-                    } //  ..  setTimeout()
-
-
+                if (crawl__pcgarage_cnt < config_pcgarage.groups.length) {
+                    crawl__establish_connection_then_parse(config_pcgarage.name, config_pcgarage.url + config_pcgarage.groups[crawl__pcgarage_cnt].url, config_pcgarage.groups[crawl__pcgarage_cnt].product_type);
+                } else {
+                    clearInterval(crawl__call_interval_pcgarage);
                 }
-            }
-        } else {
-            LOG__to_console_and_file('e', 'g', 'Error in config.jason file | 2');
-        }
+            }, interval);
+        }, 0);
 
-    }, 3000)
+        /*setTimeout(function() {
+            crawl__call_interval_emag = setInterval(function() {
+                crawl__emag_cnt++;
+
+                if (crawl__emag_cnt < config_emag.groups.length) {
+                    crawl__establish_connection_then_parse(config_pcgarage.name, config_pcgarage.url + config_pcgarage.groups[crawl__emag_cnt].url, config_pcgarage.groups[crawl__emag_cnt].product_type);
+                } else {
+                    clearInterval(crawl__call_interval_emag);
+                }
+            }, interval);
+        }, 500);*/
+    }
 }
 
 /**
- * Function used for establishing the connection between webpages from config.json and this application
+ * Function used for establishing the connection between webpages from config_pcgarage.json and this application
  * reconnect_attempts should be grater or equal to 0 !
  */
 function crawl__establish_connection_then_parse(domain_name, base_url, product_type) {
@@ -96,14 +82,14 @@ function crawl__establish_connection_then_parse(domain_name, base_url, product_t
             let x = 0;
 
             switch (domain_name) {
-                case 'pc_garage':
+                case 'pcgarage':
                     price_list = crawl__parse_pcgarage($, product_type);
                     break;
                 case 'emag':
                     price_list = crawl__parse_emag($, product_type);
                     break;
                 default:
-                    LOG__to_console_and_file('e', 'g', 'Error in config.jason file | 1');
+                    LOG__to_console_and_file('e', 'g', 'Error in config_' + domain_name + '.jason file | 3');
                     break;
             }
             return (price_list);
@@ -128,7 +114,7 @@ function crawl__establish_connection_then_parse(domain_name, base_url, product_t
  * EMAG detect when a big activity is made from a specific IP ! @todo Add a proxy connection to this site
  */
 function crawl__parse_emag($, product_type) {
-    let price_list = [];
+    /*let price_list = [];
 
     let x = 0;
     //*[@id="products-holder"]/div[2]/form/div[2] // a href
@@ -147,13 +133,13 @@ function crawl__parse_emag($, product_type) {
         x++;
     });
 
-    return (price_list);
+    return (price_list);*/
 }
 
 /**
  * Specific function for www.pcgarage.ro crawling
  * This function will parse the content of www.pcgarage.ro and crawl for specific objects
- * We will use config.json to know for what are we looking for
+ * We will use config_pcgarage.json to know for what are we looking for
  * To parse the content we will use XPath & Cheerio
  */
 function crawl__parse_pcgarage($, product_type) {
@@ -171,10 +157,9 @@ function crawl__parse_pcgarage($, product_type) {
             price_list[x][3] = 'on stock: ' + 'no';
         } else {
             price_list[x][3] = 'read error';
-            LOG__to_console_and_file('e', 'pc_garage', 'Checking the stock availablity failed');
-            return;
+            LOG__to_console_and_file('e', config_pcgarage.name, 'Checking the stock availablity failed');
+            //return;
         }
-
         x++;
     });
 
@@ -228,7 +213,7 @@ function LOG__to_console_and_file(log_type, page, message) {
         case "emag":
             message = 'EMAG | ' + message;
             break;
-        case "pc_garage":
+        case "pcgarage":
             message = 'PC-GARAGE | ' + message;
             break;
         case "g":
@@ -263,13 +248,61 @@ function LOG__to_console_and_file(log_type, page, message) {
             logger.error("Error in log function | Incorrect printing to the log file");
             break;
     }
+}
 
-    //console.log(message);
+function crawl__check_config_files() {
+    var return_check = false;
+
+    // PC-garage
+    if (config_pcgarage !== undefined || config_pcgarage.name !== undefined || config_pcgarage.url !== undefined || config_pcgarage.groups !== undefined) {
+        var i = 0;
+        while (i < config_pcgarage.groups.length) {
+            if (config_pcgarage.groups[i].product_type !== undefined ||
+                config_pcgarage.groups[i].name !== undefined ||
+                config_pcgarage.groups[i].url !== undefined ||
+                config_pcgarage.groups[i].first_page_format !== undefined ||
+                config_pcgarage.groups[i].second_page_format !== undefined) {
+                return_check = true;
+            } else {
+                LOG__to_console_and_file('e', 'g', 'Error in config_pcgarage.jason file | 2');
+                return false;
+            }
+
+            i++;
+        }
+    } else {
+        LOG__to_console_and_file('e', 'g', 'Error in config_pcgarage.jason file | 1');
+        return false;
+    }
+
+    //Emag
+    if (config_emag !== undefined || config_emag.name !== undefined || config_emag.url !== undefined || config_emag.groups !== undefined) {
+        var i = 0;
+        while (i < config_emag.groups.length) {
+            if (config_emag.groups[i].product_type !== undefined ||
+                config_emag.groups[i].name !== undefined ||
+                config_emag.groups[i].url !== undefined ||
+                config_emag.groups[i].first_page_format !== undefined ||
+                config_emag.groups[i].second_page_format !== undefined)
+                return_check &= true;
+            else {
+                LOG__to_console_and_file('e', 'g', 'Error in config_emag.jason file | 2');
+                return false;
+            }
+
+            i++;
+        }
+    } else {
+        LOG__to_console_and_file('e', 'g', 'Error in config_emag.jason file | 1');
+        return false;
+    }
+
+    return return_check;
 }
 
 /**
  * Export constructors
  */
-module.exports = crawl;
+module.exports = crawl__main;
 
-crawl();
+crawl__main();
